6ad11b26c836a2c87e196c1f77ede837f99e99c8
Aria Stewart <aredridel@dinhe.net>
2016-04-03 09:44:06 -0400

    Move slack client into its own module

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-const fetch = require('node-fetch');
-const debug = require('util').debuglog('whoa-nelly');
-const qs = require('querystring');
-const VError = require('verror');
 const fs = require('mz/fs');
 const path = require('path');
 
-if (!process.env.SLACK_TOKEN) {
-    throw new Error("SLACK_TOKEN must exist in the environment");
-}
-
 const message = fs.readFile(path.resolve(__dirname, 'whoa-message.md'), 'utf-8');
+
+const slack = require('./slack');
 
 module.exports = function whoa(user) {
     return slack.users.getByName(user).then(user => slack.channels.list()
@@ -20,38 +14,3 @@ module.exports = function whoa(user) {
                                                                                       icon_emoji: ':exclamation:'}))),
         ].concat(channels.map(c => slack.channels.kick(c.id, user.id))))))
 }
-
-function slackFetch(url, args) {
-    debug('fetching %j %j', url, args);
-    return fetch(url + "?" + qs.stringify(Object.assign({ token: process.env.SLACK_TOKEN }, args)))
-        .then(res => {
-            debug("response status %j", res.status)
-            if (res.status >= 400) {
-                throw new VError("Unexpected error %j", res.status);
-            }
-            return res.json()
-        })
-        .then(body => {
-            debug("got %j", body);
-            if (body.ok) return body;
-            throw new Error(body.error);
-        });
-}
-
-const slack = {
-    users: {
-        getByName: name => slack.users.list().then(list => list.members.find(e => e.name == name)),
-        list: () => slackFetch(`https://slack.com/api/users.list`)
-    },
-    chat: {
-        postMessage: (channel, text, opts) => slackFetch(`https://slack.com/api/chat.postMessage`, Object.assign({ channel, text }, opts))
-    },
-    channels: {
-        list: () => slackFetch(`https://slack.com/api/channels.list`),
-        kick: (channel, user) => slackFetch(`https://slack.com/api/channels.kick`, { channel, user }),
-        info: channel => slackFetch(`https://slack.com/api/channels.info`, { channel })
-    },
-    im: {
-        open: user => slackFetch(`https://slack.com/api/im.open`, { user })
-    }
-};
