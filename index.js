3b610abcab64dce31ef5f3a1253ddc61327e877d
Aria Stewart <aredridel@dinhe.net>
2016-04-04 21:49:59 -0600

    Icon as URL

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const slack = require('./slack');
 module.exports = function whoa(user, context) {
     const notify = process.env.NOTIFY_CHANNEL;
     const botname = process.env.BOT_NAME || 'friendly_admin_bot';
-    const boticon = process.env.BOT_ICON || ':adminbot:';
+    const boticon = process.env.BOT_ICON || 'https://raw.githubusercontent.com/lgbtq-technology/whoa-nelly/master/adminbot-48x48.jpeg';
     const notification = fmt("Whoa on %j by %s", user, context.user_name) + (context.channel_name ? fmt(" in %s", context.channel_name) : "");
 
     return Promise.resolve(notify && slack.chat.postMessage(notify, notification, { username: botname }))
@@ -18,7 +18,7 @@ module.exports = function whoa(user, context) {
         .then(channels => Promise.all([
             message.then(m => slack.im.open(user.id).then(im => slack.chat.postMessage(im.channel.id, m, {
                                                                                       username: botname,
-                                                                                      icon_emoji: boticon }))),
+                                                                                      icon_url: boticon }))),
         ].concat(channels.map(c => slack.channels.kick(c.id, user.id)))))))
         .then(() => "Whoa sent!")
 }
